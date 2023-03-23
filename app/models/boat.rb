3be73d7599a59c7b0fb2dee08d5c52c7f67de269
Younes Bouchamma <bouchamma.younes@gmail.com>
2023-03-23 23:18:33 +0000

    search feature

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,11 @@
 class Boat < ApplicationRecord
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_category,
+  against: [ :category ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_23_141055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_141055) do
     t.datetime "updated_at", null: false
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -85,9 +84,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_141055) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "bookings"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
