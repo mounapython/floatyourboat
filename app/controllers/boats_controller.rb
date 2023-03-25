3d4bf1780830feb946609d613e54ad43b914de3b
Mouna Rahouti <rahouti.mouna@gmail.com>
2023-03-25 14:31:09 +0000

    added mapbox

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,6 +1,14 @@
 class BoatsController < ApplicationController
   def index
     @boats= Boat.all
+    @markers = @boats.geocoded.map do |boat|
+      {
+        lat: boat.latitude,
+        lng: boat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {boat: boat}),
+        marker_html: render_to_string(partial: "marker", locals: {boat: boat})
+      }
+    end
   end
 
   def create
