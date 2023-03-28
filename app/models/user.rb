452bfc1f8ed10714fdccbd364bce8fdbb9e0e954
Mouna Rahouti <rahouti.mouna@gmail.com>
2023-03-28 22:30:05 +0000

    added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, :last_name, presence: true
   has_many :bookings
   has_many :boats, through: :bookings
 end
