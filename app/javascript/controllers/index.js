da860bc543665eb9d4c859c9329a8cea845503eb
Said Zouagui <saidzouagui@gmail.com>
2023-04-01 12:32:05 +0000

    start rating js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -13,5 +13,8 @@ application.register("hello", HelloController)
 import MapController from "./map_controller"
 application.register("map", MapController)
 
+import StarRatingController from "./star_rating_controller"
+application.register("star-rating", StarRatingController)
+
 import TypedController from "./typed_controller"
 application.register("typed", TypedController)
